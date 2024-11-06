d321c50e34c98cd13155ab9651d8ec83a31eca0c
Jhonatan Kennedy <jhonatankennedy@live.com>
2024-11-05 22:19:20 -0300

    finished

---
+++ node/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const port = 3000;
+
+const conn = mysql.createConnection({
+    host: "nodedb",
+    user: "root",
+    password: "root",
+    database: "nodedb",
+});
+
+app.get("/", (req, res) => {
+    const insertName = `INSERT INTO people(name) values('Jhonatan')`;
+    const selectNames = `SELECT * FROM people`;
+    conn.query(insertName);
+
+    conn.query(selectNames, (err, result) => {
+        const html = `
+            <h1>Full Cycle Rocks!</h1>
+            <ul>
+                ${result.map((person) => `<li>${person.name}</li>\n`).join("")}
+            </ul>
+        `;
+        res.send(html);
+    });
+});
+
+app.listen(port, () => {
+    console.log("connected to port " + port);
+});
